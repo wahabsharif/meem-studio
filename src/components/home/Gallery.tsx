ba0a88bbcd55afeddc4f70cdeb0c8ced6a912693
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-07-31 12:25:00 +0500

    Basic Settings

--- src/components/home/Gallery.tsx
+++ src/components/home/Gallery.tsx
@@ -25,9 +25,9 @@ export function Gallery() {
   return (
     <section className="p-6">
       <BlurFade delay={0.25} inView>
-        <h2 className="text-3xl text-center font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+        {/* <h2 className="text-3xl text-center font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
           Gallery
-        </h2>
+        </h2> */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4 mt-8">
           {shuffledGalleryData.map((item, index) => (
             <div
